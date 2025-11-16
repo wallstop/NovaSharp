94415f586b9e80866c2358320a39518dd6bdc432
wallstop <wallstop@wallstopstudios.com>
2025-11-15 21:21:21 -0800

    PLAN updates

--- src/tests/NovaSharp.Interpreter.Tests/Units/BreakStatementTests.cs
+++ src/tests/NovaSharp.Interpreter.Tests/Units/BreakStatementTests.cs
@@ -64,5 +64,67 @@ namespace NovaSharp.Interpreter.Tests.Units
                 "Break statements cannot cross function boundaries even when defined inside loops."
             );
         }
+
+        [Test]
+        public void MultipleBreakStatementsRespectInnermostLoopScope()
+        {
+            Script script = new Script();
+            const string chunk = @"
+                local log = {}
+
+                local function emit(flag)
+                    while true do
+                        if flag then
+                            table.insert(log, 'first')
+                            break
+                        end
+
+                        table.insert(log, 'second')
+                        break
+                    end
+                end
+
+                emit(true)
+                emit(false)
+                return log
+            ";
+
+            DynValue result = script.DoString(chunk);
+            Table log = result.Table;
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(log.Length, Is.EqualTo(2));
+                Assert.That(log.Get(1).String, Is.EqualTo("first"));
+                Assert.That(log.Get(2).String, Is.EqualTo("second"));
+            });
+        }
+
+        [Test]
+        public void BreakOnlyExitsInnermostLoop()
+        {
+            Script script = new Script();
+            const string chunk = @"
+                local log = {}
+                for outer = 1, 3 do
+                    for inner = 1, 3 do
+                        table.insert(log, string.format('%d-%d', outer, inner))
+                        break
+                    end
+                end
+                return log
+            ";
+
+            DynValue result = script.DoString(chunk);
+            Table log = result.Table;
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(log.Length, Is.EqualTo(3));
+                Assert.That(log.Get(1).String, Is.EqualTo("1-1"));
+                Assert.That(log.Get(2).String, Is.EqualTo("2-1"));
+                Assert.That(log.Get(3).String, Is.EqualTo("3-1"));
+            });
+        }
     }
 }
