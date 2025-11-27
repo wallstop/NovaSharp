4f97a00f9d2f80b04c4070ca0958d32dfae42862
wallstop <wallstop@wallstopstudios.com>
2025-11-27 12:50:00 -0800

    More tests

--- src/tests/NovaSharp.Interpreter.Tests/Units/ScriptLoaderBaseTests.cs
+++ src/tests/NovaSharp.Interpreter.Tests/Units/ScriptLoaderBaseTests.cs
@@ -51,6 +51,91 @@ namespace NovaSharp.Interpreter.Tests.Units
             Assert.That(resolved, Is.Null);
         }
 
+        [Test]
+        public void ResolveModuleNameThrowsWhenModuleNameNull()
+        {
+            Script script = new();
+            TestScriptLoader loader = new();
+
+            Assert.That(
+                () => loader.ResolveModuleName(null, script.Globals),
+                Throws.ArgumentNullException.With.Property("ParamName").EqualTo("modname")
+            );
+        }
+
+        [Test]
+        public void ResolveModuleNameUsesModulePathsWhenLuaPathIgnored()
+        {
+            Script script = new();
+            TestScriptLoader loader = new()
+            {
+                IgnoreLuaPathGlobal = true,
+                ModulePaths = new[] { "mods/?.lua" },
+            };
+            loader.AddExisting("mods/sample.lua");
+
+            string resolved = loader.ResolveModuleName("sample", script.Globals);
+
+            Assert.That(resolved, Is.EqualTo("mods/sample.lua"));
+        }
+
+        [Test]
+        public void ResolveModuleNameUsesLuaPathGlobalWhenAllowed()
+        {
+            Script script = new();
+            script.Globals.Set("LUA_PATH", DynValue.NewString("lib/?.lua"));
+
+            TestScriptLoader loader = new();
+            loader.AddExisting("lib/widget.lua");
+
+            string resolved = loader.ResolveModuleName("widget", script.Globals);
+
+            Assert.That(resolved, Is.EqualTo("lib/widget.lua"));
+        }
+
+        [Test]
+        public void ResolveModuleNameThrowsWhenGlobalContextNull()
+        {
+            TestScriptLoader loader = new();
+
+            Assert.That(
+                () => loader.ResolveModuleName("module", null),
+                Throws.ArgumentNullException.With.Property("ParamName").EqualTo("globalContext")
+            );
+        }
+
+        [Test]
+        public void ResolveModuleNameReturnsNullWhenPathListNull()
+        {
+            TestScriptLoader loader = new();
+
+            string resolved = loader.ResolveFromPaths("foo", null);
+
+            Assert.That(resolved, Is.Null);
+        }
+
+        [Test]
+        public void ResolveFileNameTrimsWhitespace()
+        {
+            Script script = new();
+            TestScriptLoader loader = new();
+
+            string resolved = loader.ResolveFileName("  foo.lua  ", script.Globals);
+
+            Assert.That(resolved, Is.EqualTo("foo.lua"));
+        }
+
+        [Test]
+        public void ResolveModuleNameNormalizesDottedModules()
+        {
+            TestScriptLoader loader = new();
+            loader.AddExisting("modules/foo/bar.lua");
+
+            string resolved = loader.ResolveFromPaths("foo.bar", "modules/?.lua");
+
+            Assert.That(resolved, Is.EqualTo("modules/foo/bar.lua"));
+        }
+
         private sealed class TestScriptLoader : ScriptLoaderBase
         {
             private readonly HashSet<string> _existing = new(StringComparer.Ordinal);
